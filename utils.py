865658c926c33c401ab491bb233424b2d01ea6cf
PeterB <byfield554@gmail.com>
2017-12-04 15:58:05 +0000

    ok

--- utils.py
+++ utils.py
@@ -9,7 +9,52 @@ Use with python via e.g https://anaconda.org/conda-forge/python-spams
 import numpy as np
 import cv2 as cv
 import spams
+import matplotlib.pyplot as plt
 
+
+def read_image(path):
+    """
+    Read an image to RGB uint8
+    :param path:
+    :return:
+    """
+    im = cv.imread(path)
+    im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
+    return im
+
+
+def show_colors(C):
+    """
+    Shows rows of C as colors (RGB)
+    :param C:
+    :return:
+    """
+    n = C.shape[0]
+    for i in range(n):
+        if C[i].max() > 1.0:
+            plt.plot([0, 1], [n - 1 - i, n - 1 - i], c=C[i] / 255, linewidth=20)
+        else:
+            plt.plot([0, 1], [n - 1 - i, n - 1 - i], c=C[i], linewidth=20)
+        plt.axis('off')
+        plt.axis([0, 1, -1, n])
+
+def show(image, now=True, fig_size=(10, 10)):
+    """
+    Show an image (np.array).
+    Caution! Rescales image to be in range [0,1].
+    :param image:
+    :param now:
+    :param fig_size:
+    :return:
+    """
+    image = image.astype(np.float32)
+    m, M = image.min(), image.max()
+    if fig_size != None:
+        plt.rcParams['figure.figsize'] = (fig_size[0], fig_size[1])
+    plt.imshow((image - m) / (M - m), cmap='gray')
+    plt.axis('off')
+    if now == True:
+        plt.show()
 
 def remove_zeros(I):
     """
