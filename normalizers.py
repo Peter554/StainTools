adfb2720d60eed2a28587ebabfd95ddda4da5fa4
Unknown <byfield554@gmail.com>
2018-04-06 21:03:00 +0100

    abc?

--- normalizers.py
+++ normalizers.py
@@ -11,9 +11,9 @@ import cv2 as cv
 import numpy as np
 from utils import misc_utils as mu
 import spams
+from normalizer_abc import Normaliser, FancyNormalizer
 
-
-class ReinhardNormalizer(object):
+class ReinhardNormalizer(Normaliser):
     """
     Normalize a patch stain to the target image using the method of:
     E. Reinhard, M. Adhikhmin, B. Gooch, and P. Shirley, ‘Color transfer between images’, IEEE Computer Graphics and Applications, vol. 21, no. 5, pp. 34–41, Sep. 2001.
@@ -93,54 +93,7 @@ class ReinhardNormalizer(object):
         return means, stds
 
 
-class BaseNormalizer(object):
-
-    def target_stains(self):
-        """
-        Get target stains as RGB
-        :return:
-        """
-        return mu.OD_to_RGB(self.stain_matrix_target)
-
-    def hematoxylin(self, I):
-        """
-        Hematoxylin channel
-        :param I:
-        :return:
-        """
-        I = mu.standardize_brightness(I)
-        h, w, c = I.shape
-        stain_matrix_source = self.get_stain_matrix(I)
-        source_concentrations = self.get_concentrations(I, stain_matrix_source)
-        H = source_concentrations[:, 0].reshape(h, w)
-        H = np.exp(-1 * H)
-        return H
-
-    @staticmethod
-    def get_concentrations(I, stain_matrix, lamda=0.01):
-        """
-        Get the concentration matrix. Suppose the input image is H x W x 3 (uint8). Define Npix = H * W.
-        Then the concentration matrix is Npix x 2 (or we could reshape to H x W x 2).
-        The first element of each row is the Hematoxylin concentration.
-        The second element of each row is the Eosin concentration.
-
-        We do this by 'solving' OD = C*S (Matrix product) where OD is optical density (Npix x 3),\
-        C is concentration (Npix x 2) and S is stain matrix (2 x 3).
-        See docs for spams.lasso.
-
-        We restrict the concentrations to be positive and penalise very large concentration values,\
-        so that background pixels (which can not easily be expressed in the Hematoxylin-Eosin basis) have \
-        low concentration and thus appear white.
-
-        :param I: Image. A np array HxWx3 of type uint8.
-        :param stain_matrix: a 2x3 stain matrix. First row is Hematoxylin stain vector, second row is Eosin stain vector.
-        :return:
-        """
-        OD = mu.RGB_to_OD(I).reshape((-1, 3))  # convert to optical density and flatten to (H*W)x3.
-        return spams.lasso(OD.T, D=stain_matrix.T, mode=2, lambda1=lamda, pos=True).toarray().T
-
-
-class MacenkoNormalizer(BaseNormalizer):
+class MacenkoNormalizer(FancyNormalizer):
     """
     Stain normalization based on the method of:
     M. Macenko et al., ‘A method for normalizing histology slides for quantitative analysis’, in 2009 IEEE International Symposium on Biomedical Imaging: From Nano to Macro, 2009, pp. 1107–1110.
@@ -204,7 +157,7 @@ class MacenkoNormalizer(BaseNormalizer):
         return mu.normalize_rows(HE)
 
 
-class VahadaneNormalizer(BaseNormalizer):
+class VahadaneNormalizer(FancyNormalizer):
     """
     Stain normalization inspired by method of:
     A. Vahadane et al., ‘Structure-Preserving Color Normalization and Sparse Stain Separation for Histological Images’, IEEE Transactions on Medical Imaging, vol. 35, no. 8, pp. 1962–1971, Aug. 2016.
