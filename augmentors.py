b9d26873c05d5558a4a2e70ca813e960e55c1650
Peter554 <byfield554@gmail.com>
2018-04-06 14:16:52 +0100

    Refactored Augmentor with Fetcher class. Also split into fitting and augmenting to save time when augmenting. And don't augment white pixels.

--- augmentors.py
+++ augmentors.py
@@ -1,124 +1,162 @@
 """
-Augmentors objects
+Stain augmentation objects
 """
+
+from __future__ import division
+
 import numpy as np
+import normalizers
 from utils import misc_utils as mu
+import copy
 
-def RJ_concentrations(I):
-    """
-    Performs stain concentartion extraction according to
-    A. C. Ruifrok, D. A. Johnston et al., “Quantification of histochemical
-    staining by color deconvolution,” Analytical and quantitative cytology
-    and histology, vol. 23, no. 4, pp. 291–299, 2001.
-    """
-    OD = mu.RGB_to_OD(I).reshape((-1, 3))
 
-    stain_matrix = np.array([[0.644211, 0.716556, 0.266844],
-          [0.092789, 0.954111, 0.283111],
-          [-0.0903,-0.2752, 0.9571]])
+class Fetcher(object):
 
-    source_concentrations = np.dot(OD, np.linalg.inv(stain_matrix))
-
-    return source_concentrations, stain_matrix
-
-class Fancy_concentrations(object):
-    """
-    Performs stain concentration exctraction according to
-    Macenko or Vahadane
-    :param method; type normalizers object
-    :return: concentration (Npix x 2)
-    """
     def __init__(self, method):
-        self.method = method
+        """
+        Object to fetch stain matrix and concentrations given a method
+        :param method: one of 'RJ', 'Macenko', 'Vahadane'.
+        """
+        assert method in ['RJ', 'Macenko', 'Vahadane'], 'select appropriate method!'
+        if method == 'RJ':
+            self.stain_fetcher = self.RJ_stain
+            self.concentration_fetcher = self.RJ_concentrations
+        else:
+            if method == 'Macenko':
+                normalizer = normalizers.MacenkoNormalizer
+            if method == 'Vahadane':
+                normalizer = normalizers.MacenkoNormalizer
+            self.stain_fetcher = normalizer.get_stain_matrix
+            self.concentration_fetcher = normalizer.get_concentrations
 
     def compute(self, I, just_stain=False):
         """
-        By default returns stain_matrix and concentrations
+        By default returns concentrations and stain_matrix
         To compute just stain_matrix set just_stain to True
+        :param I:
+        :param just_stain:
+        :return:
         """
-        stain_matrix = self.method.get_stain_matrix(I)
-        if just_stain == True:
+        stain_matrix = self.stain_fetcher(I)
+        if just_stain:
             return stain_matrix
         else:
-            source_concentrations = self.method.get_concentrations(I, stain_matrix)
-            return source_concentrations, stain_matrix
+            source_concentrations = self.concentration_fetcher(I, stain_matrix)
+            return stain_matrix, source_concentrations
 
-def assign_method(method):
-    """
-    Tests and returns augmentation method
-    """
-    assert method in ['RJ', 'Macenko', 'Vahadane'], 'select appropriate method!'
-
-    if method == 'RJ':
-        return RJ_concentrations
-    if method == 'Macenko':
-        from normalizers import MacenkoNormalizer
-        return Fancy_concentrations(MacenkoNormalizer).compute
-    if method == 'Vahadane':
-        from normalizers import VahadaneNormalizer
-        return Fancy_concentrations(VahadaneNormalizer).compute
-
-class Augmentor(object):
-    """
-    Augment a patch or a directory with patches according to method
-    described in:
-    Tellez, D., M. Balkenhol, I. Otte-Höller, R. van de Loo, R. Vogels, P. Bult,
-    C. Wauters, et al. “Whole-Slide Mitosis Detection in H #x0026;E Breast Histology
-    Using PHH3 as a Reference to Train Distilled Stain-Invariant Convolutional Networks.”
-    IEEE Transactions on Medical Imaging PP, no. 99 (2018): 1–1.
-    """
-
-    def __init__(self, method='RJ', sigma1=0.3, sigma2=0.3):
+    @staticmethod
+    def RJ_stain(*args):
         """
-        Initialise by specifying which stain matrix and source estimation method to use
-        :param stain_matrix_method: type str; 'RJ' 'Macenko' 'Vahadane'
+        Get RJ stain matrix.
+        A. C. Ruifrok, D. A. Johnston et al., “Quantification of histochemical
+        staining by color deconvolution,” Analytical and quantitative cytology
+        and histology, vol. 23, no. 4, pp. 291–299, 2001.
+        :param args: a dummy
+        :return:
         """
+        stain_matrix = np.array([[0.644211, 0.716556, 0.266844],
+                                 [0.092789, 0.954111, 0.283111],
+                                 [-0.0903, -0.2752, 0.9571]])
+        return stain_matrix
 
-        self.method = assign_method(method)
+    def RJ_concentrations(self, I, stain_matrix):
+        """
+        Performs stain concentration extraction according to
+        A. C. Ruifrok, D. A. Johnston et al., “Quantification of histochemical
+        staining by color deconvolution,” Analytical and quantitative cytology
+        and histology, vol. 23, no. 4, pp. 291–299, 2001.
+        :param I:
+        :return:
+        """
+        OD = mu.RGB_to_OD(I).reshape((-1, 3))
+        source_concentrations = np.dot(OD, np.linalg.inv(stain_matrix))
+        return source_concentrations
+
+
+class TellezAugmentor(object):
+
+    def __init__(self, method='RJ', sigma1=0.2, sigma2=0.2):
+        """
+        Augment a patch according to method described in:
+        Tellez, D., M. Balkenhol, I. Otte-Höller, R. van de Loo, R. Vogels, P. Bult,
+        C. Wauters, et al. “Whole-Slide Mitosis Detection in H&E Breast Histology
+        Using PHH3 as a Reference to Train Distilled Stain-Invariant Convolutional Networks.”
+        IEEE Transactions on Medical Imaging PP, no. 99 (2018): 1–1.
+        :param method: one of 'RJ', 'Macenko', 'Vahadane'.
+        :param sigma1:
+        :param sigma2:
+        """
+        self.fetcher = Fetcher(method)
         self.sigma1 = sigma1
         self.sigma2 = sigma2
 
-    def augment(self, I, new_stain_mat=False):
+    def fit(self, I, standardize_brightness=True):
         """
-        Get concentrations and return augmented image
-        :param I; image to be augmented
+        Fit the augmentor to an image I
+        :param I:
+        :return:
+        """
+        if standardize_brightness:
+            I = mu.standardize_brightness(I)
+        self.Ishape = I.shape
+        self.not_white = mu.notwhite_mask(I).reshape(-1)
+        self.stain_matrix, self.source_concentrations = self.fetcher.compute(I)
+
+    def augment(self, new_stain_mat=False):
+        """
+        Return augmented image.
+        Optionally returns new stain matrix
         :param new_stain_mat; type bool, if True computes & returns new stain matrix
         """
-        source_concentrations, stain_matrix = self.method(I)
-
-        iter = source_concentrations.shape[1]
-        for i in range(iter):
-            alpha = np.random.uniform(1-self.sigma1, 1+self.sigma1)
+        channels = self.source_concentrations.shape[1]
+        source_concentrations = copy.deepcopy(self.source_concentrations)
+        for i in range(channels):
+            alpha = np.random.uniform(1 - self.sigma1, 1 + self.sigma1)
             beta = np.random.uniform(-self.sigma2, self.sigma2)
-            source_concentrations[:,i] *= alpha
-            source_concentrations[:,i] += beta
+            source_concentrations[self.not_white, i] *= alpha
+            source_concentrations[self.not_white, i] += beta
 
-        I_prime = np.clip((255 * np.exp(-1 * np.dot(source_concentrations, stain_matrix).reshape(I.shape))), 0, 255).astype(
-            np.uint8)
+        I_prime = np.clip((255 * np.exp(-1 * np.dot(source_concentrations, self.stain_matrix).reshape(self.Ishape))), 0,
+                          255).astype(np.uint8)
 
         if new_stain_mat:
-            stain_matrix = self.method(I, just_stain=True)
+            stain_matrix = self.fetcher.compute(I_prime, just_stain=True)
             return I_prime, stain_matrix
         else:
             return I_prime
 
+
 if __name__ == '__main__':
-    import cv2
     import matplotlib.pyplot as plt
-    from utils.visual_utils import read_image
-    I = read_image('data/i1.png')
+    from utils import visual_utils as vu
+
+    I = vu.read_image('data/i1.png')
+
+    # Test 0
+    augmentor = TellezAugmentor('Vahadane')
+    augmentor.fit(I)
+    print('Fitting done')
+    h, w, c = I.shape
+    stack = np.zeros([10, h, w, c])
+    for i in range(10):
+        stack[i] = augmentor.augment(new_stain_mat=False)
+    vu.patch_grid(stack, width=5)
+
     # Test 1
+    for method in ['Macenko', 'Vahadane']:
+        augmentor = TellezAugmentor(method=method)
+        augmentor.fit(I)
+        I_prime, stain_matrix = augmentor.augment(new_stain_mat=True)
+
+    # Test 2
     for method in ['RJ', 'Macenko', 'Vahadane', 'test']:
         try:
-            augmentor = Augmentor(method=method)
-            I_prime = augmentor.augment(I)
-            f, axarr = plt.subplots(1,2)
+            augmentor = TellezAugmentor(method=method)
+            augmentor.fit(I)
+            I_prime = augmentor.augment()
+            f, axarr = plt.subplots(1, 2)
             axarr[0].imshow(I)
             axarr[1].imshow(I_prime)
             plt.show()
         except AssertionError:
-            print('Cought faulty method, YAY!')
-    # Tets 2
-    for method in ['Macenko', 'Vahadane']:
-        augmentor = Augmentor(method=method)
-        I_prime, stain_matrix = augmentor.augment(I, new_stain_mat=True)
+            print('Cought faulty method, YAY!')  # exception ends run ?
