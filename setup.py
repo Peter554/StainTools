6057f65835fb0d04334c16540dfd34915a4ef25f
Unknown <byfield554@gmail.com>
2018-05-16 22:24:36 +0100

    packaging

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('README.md') as f:
     readme = f.read()
 
 setup(
     name='staintools',
-    version='0.0.1',
+    version='0.0.2',
     description='A package for stain normalization, augmentation and more.',
     long_description=readme,
     author='Peter Byfield',
     author_email='byfield554@gmail.com',
     url='https://github.com/Peter554/StainTools',
-    packages=['staintools'],
+    packages=find_packages(),
     install_requires=['numpy',
                       'opencv-python',
                       'matplotlib',
