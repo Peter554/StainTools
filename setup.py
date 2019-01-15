3d41685d788f538c65182926c65c436957376590
Peter Byfield <byfield554@gmail.com>
2019-01-15 20:51:04 +0000

    Bump version: 2.0.1 → 2.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='staintools',
-    version='2.0.1',
+    version='2.1.0',
     description='A package for tissue image stain normalization, augmentation and more.',
     long_description=readme,
     author='Peter Byfield',
