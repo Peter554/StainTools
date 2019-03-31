fd4000371c6c4be30534b33e1f87371d7941084b
Peter Byfield <byfield554@gmail.com>
2019-03-31 19:21:06 +0100

    Basic tidying

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
-    readme = f.read()
-
 setup(
     name='staintools',
     version='2.1.1',
     description='A package for tissue image stain normalization, augmentation and more.',
-    long_description=readme,
     author='Peter Byfield',
     author_email='byfield554@gmail.com',
     url='https://github.com/Peter554/StainTools',
     packages=find_packages(exclude=('tests')),
     install_requires=[
         'numpy',
+        'matplotlib',
         'opencv-python',
-        'spams',
-        'matplotlib'
+        'spams'
     ]
 )
